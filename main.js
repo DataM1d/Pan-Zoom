3fd39bdf2e5fdc4f0c3659e98e90410269f22388
DataM1d <minadze18@icloud.com>
2025-10-04 15:04:23 +0200

    main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+myCanvas.width = 600;
+         myCanvas.height = 600;
+
+         const ctx = myCanvas.getContext("2d");
+
+         const graphString = localStorage.getItem('graph');
+         const graphInfo = graphString ? JSON.parse(graphString) : null;
+         const graph = graphInfo ? Graph.load(graphInfo) : new Graph();
+         const viewport = new Viewport(myCanvas);
+         const graphEditor = new GraphEditor(viewport, graph);
+
+         animate();
+
+         function animate() {
+            viewport.reset();
+            graphEditor.display();
+            requestAnimationFrame(animate);
+         }
+
+         function dispose() {
+            graphEditor.dispose();
+         }
+
+         function save() {
+            localStorage.setItem('graph', JSON.stringify(graph));
+         }
